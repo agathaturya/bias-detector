1b855be4b41aa77b6d6fec8c4138ae5ff9d8a481
Agatha Turyahikayo <agathaturyahikayo14@gmail.com>
2018-02-11 00:24:56 -0500

    Merge branch 'master' of https://github.com/agathaturya/bias-detector

--- content.js
+++ content.js
@@ -1,19 +1,37 @@
 var currentURL=document.URL;
-
-if (currrentURL.indexOf("twitter")>-1){
-
-var MICROSOFT_API_KEY="";
+var  MICROSOFT_API_KEY="";
 var GOOGLE_API="";
 
+console.log(currentURL);
 
-//getting api keys from config
-http('GET', chrome.runtime.getURL('config.json'), '', function(obj)){
-MICROSOFT_API_KEY=obj.microsoft_key;
-GOOGLE_API=obj.google_key;
-}
+if (currentURL.indexOf("twitter")>-1){
 
+var http = function (method, url, body, cb) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url, true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== 4) { return; }
+    if (xhr.status >= 400) {
+      notify('API request failed');
+      console.log('XHR failed', xhr.responseText);
+      return;
+    }
+  cb(JSON.parse(xhr.responseText));
+  };
+  xhr.send(body);
+};
 
-//gets all images on the page
-var images=document.getElementsByTagName('img');
-var text
+http('GET', chrome.runtime.getURL('config.json'), '', function (obj) {
+    MICROSOFT_API_KEY=obj.microsoft_key;
+    GOOGLE_API=obj.google_key;
+    document.addEventListener('DOMContentLoaded', checkTweets(), false);
+
+});
+function checkTweets(){
+
+  //gets all images on the page
+  var images=document.getElementsByTagName('img');
+
+  }
 }
